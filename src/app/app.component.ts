b2970bffee74ec55b205cc2de9bc26d734936b36
Peanut <halojer78@gmail.com>
2023-03-04 11:56:21 +0100

    Create toolbar without mechanics
    Added hide button for sidebar and prepared to install animations and reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,21 @@ import { Component, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  animations: [
+
+  ]
 })
 export class AppComponent {
   title = 'tasksApp';
   activeElement?: Element | null
+  sidebarCollapsed: boolean = true
+
+  colors: Object[] = [
+    { main: '', background: '', text: '' },
+    { main: '', background: '', text: '' },
+    { main: '', background: '', text: '' }
+  ]
 
   switchMode(e: Event){
     const target = e.target as Element
